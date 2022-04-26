a937b774192c797fb8cea9bbafebbf1b7119d18f
vinayakdighe <vinayakdighe31@gmail.com>
2022-04-26 23:10:00 +0530

    creating user
    user creation is not working yet

--- blogsystem/first/views.py
+++ blogsystem/first/views.py
@@ -1,20 +1,49 @@
 from colorsys import TWO_THIRD
+from pyexpat.errors import messages
+from urllib import response
 from django.shortcuts import render, HttpResponse, redirect
+from django.contrib.auth import login,authenticate
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
 def signup(request):
-   q = request.GET.get('q')
-   if q is not None:
-       return redirect('/signup/')
-   else:
-       return render(request, 'signup.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+
+
+        myuser = User.objects.create_user(username, email, pass1)
+        myuser.first_name = fname
+        myuser.last_name = lname
+        myuser.save()
+        messages.success(request, 'your account is created')
+        return render(request, 'signup.html')
+
+    else:
+        return HttpResponse('not found')
+
+    
+
+
 
 def login(request):
-   two = request.GET.get('two')
-   if two is not None:
-       return redirect('/login/')
-   else:
-       return render(request, 'login.html')
+    return render(request, 'login.html')
+
+#    two = request.GET.get('two')
+#    if two is not None:
+#        return redirect('/login/')
+#    else:
+#        return render(request, 'login.html')
+
+
+
+
